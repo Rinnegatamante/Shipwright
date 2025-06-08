503c067fc04bffd0b38d4695df383a34ab6dc50d
Rinnegatamante <rinnegatamante@gmail.com>
2025-06-08 11:20:23 +0200

    Typo fix.

--- CMake/automate-vcpkg.cmake
+++ CMake/automate-vcpkg.cmake
@@ -118,11 +118,11 @@ macro(_install_or_update_vcpkg)
         message(STATUS "Cloning vcpkg in ${VCPKG_ROOT}")
         execute_process(COMMAND git clone https://github.com/Microsoft/vcpkg.git ${VCPKG_ROOT} --depth 1)
 
-         If a reproducible build is desired (and potentially old libraries are # ok), uncomment the
-         following line and pin the vcpkg repository to a specific githash.
-         execute_process(COMMAND git checkout 745a0aea597771a580d0b0f4886ea1e3a94dbca6 WORKING_DIRECTORY ${VCPKG_ROOT})
+        # If a reproducible build is desired (and potentially old libraries are # ok), uncomment the
+        # following line and pin the vcpkg repository to a specific githash.
+        # execute_process(COMMAND git checkout 745a0aea597771a580d0b0f4886ea1e3a94dbca6 WORKING_DIRECTORY ${VCPKG_ROOT})
     else()
-         The following command has no effect if the vcpkg repository is in a detached head state.
+        # The following command has no effect if the vcpkg repository is in a detached head state.
         message(STATUS "Auto-updating vcpkg in ${VCPKG_ROOT}")
         execute_process(COMMAND git pull WORKING_DIRECTORY ${VCPKG_ROOT})
     endif()
