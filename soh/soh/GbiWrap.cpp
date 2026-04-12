30ecea538251d6e275a421ad60f9ac18c1f885c3
Rinnegatamante <rinnegatamante@gmail.com>
2026-04-12 20:57:01 +0200

    Fix incorecct function signature for GetResourceDataByName in GBIWrap.

--- soh/soh/GbiWrap.cpp
+++ soh/soh/GbiWrap.cpp
@@ -16,7 +16,7 @@ int32_t OTRGetLastScancode();
 void ResourceMgr_LoadDirectory(const char* resName);
 void ResourceMgr_LoadFile(const char* resName);
 char* ResourceMgr_LoadFileFromDisk(const char* filePath);
-char* GetResourceDataByName(char* texPath);
+char* GetResourceDataByName(char* texPath, bool now);
 uint16_t ResourceMgr_LoadTexWidthByName(char* texPath);
 uint16_t ResourceMgr_LoadTexHeightByName(char* texPath);
 size_t GetResourceTexSizeByName(const char* name);
